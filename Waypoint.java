2f41511c0274916b614237683d23f133046d9a36
JST5000 <aim4math@gmail.com>
2016-06-03 23:35:17 -0700

    Update Waypoint.java

--- Waypoint.java
+++ Waypoint.java
@@ -1,62 +1,45 @@
 package grid;
 
-import org.lwjgl.util.vector.Vector2f;
+import org.newdawn.slick.geom.Vector2f;
 //A way point is a path to be taken in a straight line from point a to point b.
 public class Waypoint {
 	
-	//This gives the absolute (pixel) location of the start point on the page.
-	private Vector2f locationP1;
-	
-	//This is the location of the start point in the Array of the Grid.
+	//This gives the array location of the start point on the page.
 	private Vector2f locationA1;
-	
-	//This gives the absolute (pixel) location of the end point
-	private Vector2f locationP2;
-	
-	//This is the location of the end-point with values given in row/column
-	private Vector2f locationA2;
-	
-	private Grid g;
-	//Makes a way point.
-	Waypoint(Vector2f loc1, Vector2f loc2, Grid gr) {
 		
-		//Establishes an internal connection to the grid
-		g = gr;
+	private int value;
+	
+	private Waypoint parent;
+	//Makes a way point.
+	Waypoint(Vector2f loc1) {
 		
 		//Establishes start point values
-		locationP1 = loc1;
-		locationA1 = new Vector2f((int)(loc1.getX()/(g.getSize().getX()/g.getGrid().length)),
-					(int)(loc1.getY()/(g.getSize().getY())));
+		locationA1 = loc1;
+	
+		parent = null;
 		
-		//Establishes end point values
-		locationP2 = loc2;
-		locationA2 = new Vector2f((int)(loc2.getX()/(g.getSize().getX()/g.getGrid().length)),
-					(int)(loc2.getY()/(g.getSize().getY())));
+		value = 0;
 	}
 	
 	//Returns the location of start relative to page corner in pixels
-	public Vector2f getAbsLoc1() {
-		return locationP1;
-	}
-	
-	//Returns the location of end relative to page corner in pixels
-	public Vector2f getAbsLoc2() {
-		return locationP2;
-	}
-	
-	//Returns the location of the start relative to the grid (Row, Column)
-	public Vector2f getGridLoc1() {
+	public Vector2f getLoc() {
 		return locationA1;
 	}
 	
-	//Returns the location of the end relative to the grid (Row, Column)
-
-	public Vector2f getGridLoc2() {
-		return locationA2;
+	public int getValue() {
+		return value;
 	}
 	
-	public Vector2f getDisp() {
-		return new Vector2f(locationA1.getX()-locationA2.getX(), locationA1.getY()-locationA2.getY());
+	public void setValue(int x) {
+		value = x;
+	}
+	
+	public Waypoint getParent() {
+		return parent;
+	}
+	
+	public void setParent(Waypoint p) {
+		parent = p;
 	}
 	
 }
