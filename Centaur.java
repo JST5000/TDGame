58206854ad56654d31268a4794b8e7a53577fbe7
nyako2850 <nyako2850@gmail.com>
2016-06-07 13:50:43 -0700

    Update Centaur.java

--- Centaur.java
+++ Centaur.java
@@ -1,22 +1,32 @@
-import org.newdawn.slick.geom.Vector2f;
-import org.newdawn.slick.Image;
+
+   import org.newdawn.slick.AppGameContainer;
+   import org.newdawn.slick.BasicGame;
+   import org.newdawn.slick.Color;
+   import org.newdawn.slick.GameContainer;
+   import org.newdawn.slick.Graphics;
+   import org.newdawn.slick.Image;
+   import org.newdawn.slick.Input;
+   import org.newdawn.slick.SlickException;
+   import org.newdawn.slick.Sound;
+   import org.newdawn.slick.geom.Rectangle;
+   import org.newdawn.slick.geom.Vector2f;
 
 public class Centaur extends Enemy
 {
-	public Centaur(Vector2f loc, Vector2f vel, Vector2f size)
-	throws FileNotFoundException;
-	throws IOException;
+	public Centaur(Vector2f loc, Vector2f vel, Vector2f size, Icon img)
+// 	throws FileNotFoundException;
+// 	throws IOException;
 	{
 	
-		super(false, 1, 1, 1, loc, vel, size, new Image(2, 2));
+		super(false, 10, 1, 2, loc, vel, size, img);
 // 		canFly = false;
-// 		hp = 2;   //Can be changed
+// 		hp = 10;   //Can be changed
 // 		this.vel = vel;  //Can be changed
 // 		this.size = size; //Can be changed
 // 		atk = hp;
 // 		this.loc = loc;
 // 		def = 2;  //Can be changed
-// 		Image img = new Image(/* Poopy image (Use png, that allows see through shit)*/);
+// 		Icon img = new Icon(/* Poopy Icon (Use png, that allows see through shit)*/);
 // 		this.img = img;
 	}
 }
