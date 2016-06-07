8ef82954d61bfe3b9ce5414b111b68723c299c68
nyako2850 <nyako2850@gmail.com>
2016-06-07 14:05:29 -0700

    Update Owl.java

--- Owl.java
+++ Owl.java
@@ -1,14 +1,24 @@
-import org.newdawn.slick.geom.Vector2f;
-import org.newdawn.slick.Image;
+
+   import org.newdawn.slick.AppGameContainer;
+   import org.newdawn.slick.BasicGame;
+   import org.newdawn.slick.Color;
+   import org.newdawn.slick.GameContainer;
+   import org.newdawn.slick.Graphics;
+   import org.newdawn.slick.Image;
+   import org.newdawn.slick.Input;
+   import org.newdawn.slick.SlickException;
+   import org.newdawn.slick.Sound;
+   import org.newdawn.slick.geom.Rectangle;
+   import org.newdawn.slick.geom.Vector2f;
 
 public class Owl extends Enemy
 {
-	public Owl(Vector2f loc, Vector2f vel, Vector2f size)
-	throws FileNotFoundException;
-	throws IOException;
+	public Owl(Vector2f loc, Vector2f vel, Vector2f size, Icon img)
+// 	throws FileNotFoundException;
+// 	throws IOException;
 	{
 	
-		super(true, 1, 1, 1, loc, vel, size, new Image(2, 2));
+		super(true, 1, 1, 1, loc, vel, size, img);
 // 		canFly = true;
 // 		hp = 1;   //Can be changed
 // 		this.vel = vel;  //Can be changed
@@ -16,7 +26,7 @@ public class Owl extends Enemy
 // 		atk = hp;
 // 		this.loc = loc;
 // 		def = 1;  //Can be changed
-// 		Image img = new Image(/* Poopy image (Use png, that allows see through shit)*/);
+// 		Icon img = new Icon(/* Poopy Icon (Use png, that allows see through shit)*/);
 // 		this.img = img;
 	}
 }
