9454b66d455d5e54fb8b6b5e2f981632c400f71e
nyako2850 <nyako2850@gmail.com>
2016-06-07 14:04:36 -0700

    Update HouseElf.java

--- HouseElf.java
+++ HouseElf.java
@@ -1,14 +1,25 @@
-import org.newdawn.slick.geom.Vector2f;
-import org.newdawn.slick.Image;
+
+   import org.newdawn.slick.AppGameContainer;
+   import org.newdawn.slick.BasicGame;
+   import org.newdawn.slick.Color;
+   import org.newdawn.slick.GameContainer;
+   import org.newdawn.slick.Graphics;
+   import org.newdawn.slick.Image;
+   import org.newdawn.slick.Input;
+   import org.newdawn.slick.SlickException;
+   import org.newdawn.slick.Sound;
+   import org.newdawn.slick.geom.Rectangle;
+   import org.newdawn.slick.geom.Vector2f;
+
 
 public class HouseElf extends Enemy
 {	
-	public HouseElf(Vector2f loc, Vector2f vel, Vector2f size)
-	throws FileNotFoundException;
-	throws IOException;
+	public HouseElf(Vector2f loc, Vector2f vel, Vector2f size, Icon img)
+// 	throws FileNotFoundException;
+// 	throws IOException;
 	{
 	
-		super(false, 1, 1, 1, loc, vel, size, new Image(2, 2));
+		super(false, 1, 1, 1, loc, vel, size, img);
 // 		canFly = false;
 // 		hp = 3;   //Can be changed
 // 		this.vel = vel;  //Can be changed
@@ -16,7 +27,7 @@ public class HouseElf extends Enemy
 // 		atk = hp;
 // 		this.loc = loc;
 // 		def = 3;  //Can be changed
-// 		Image img = new Image(/* Poopy image (Use png, that allows see through shit)*/);
+// 		Icon img = new Icon(/* Poopy Icon (Use png, that allows see through shit)*/);
 // 		this.img = img;
 	}
 	
