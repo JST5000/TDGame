27e36480609981a6ca141d4be56e0f85e0da34c9
nyako2850 <nyako2850@gmail.com>
2016-06-07 14:07:05 -0700

    Update Werewolf.java

--- Werewolf.java
+++ Werewolf.java
@@ -1,14 +1,24 @@
-import org.newdawn.slick.geom.Vector2f;
-import org.newdawn.slick.Image;
+
+   import org.newdawn.slick.AppGameContainer;
+   import org.newdawn.slick.BasicGame;
+   import org.newdawn.slick.Color;
+   import org.newdawn.slick.GameContainer;
+   import org.newdawn.slick.Graphics;
+   import org.newdawn.slick.Image;
+   import org.newdawn.slick.Input;
+   import org.newdawn.slick.SlickException;
+   import org.newdawn.slick.Sound;
+   import org.newdawn.slick.geom.Rectangle;
+   import org.newdawn.slick.geom.Vector2f;
 
 public class Werewolf extends Enemy
 {
-	public Werewolf(Vector2f loc, Vector2f vel, Vector2f size)
-	throws FileNotFoundException;
-	throws IOException;
+	public Werewolf(Vector2f loc, Vector2f vel, Vector2f size, Icon img)
+// 	throws FileNotFoundException;
+// 	throws IOException;
 	{
 	
-		super(false, 3, 3, 3, loc, vel, size, new Image(2, 2));
+		super(false,  30, 10, 3, loc, vel, size, img);
 // 		canFly = false;
 // 		hp = 3;   //Can be changed
 // 		this.vel = vel;  //Can be changed
@@ -16,7 +26,7 @@ public class Werewolf extends Enemy
 // 		atk = hp;
 // 		this.loc = loc;
 // 		def = 3;  //Can be changed
-// 		Image img = new Image(/* Poopy image (Use png, that allows see through shit)*/);
+// 		Icon img = new Icon(/* Poopy Icon (Use png, that allows see through shit)*/);
 // 		this.img = img;
 	}
 }
