bddd5d9e2264801b2831aa47252a4e36ff1366f8
raventooblue <ribbon.seal.90@gmail.com>
2016-06-08 21:11:50 -0700

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -4,20 +4,22 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Bullet {
-	private Icon bullet;
-	private int atk;
-	private Enemy e;
-	private int speed;
-	private boolean alive;
-	private Vector2f location;
-	private int lifespan;
-	public static int MAX_LIFETIME;
+	private Icon bullet;//design of the bullet
+	private int atk;//damage the bullet does
+	private enemy e;
+	private Vector2f speed;//how fast the bullet travels
+	private boolean alive;//whether the bullet is still functional
+	private Vector2f location;//where the bullet is 
+	private int lifespan;//how long the bullet has been alive, to avoid rendering it too long
+	public static int MAX_LIFETIME;//cap on how long the bullet could possibly exist
+	private Vector2f origin;//where the turret is; where the bullet originates
 	
-	Bullet(Icon b, int atk, Enemy e, Vector2f location) {//add enemy as a parameter to lock onto, adds location (which should be the turret)
+	Bullet(Icon b, int atk, Enemy e, Vector2f location, int angle) {//add enemy as a parameter to lock onto, adds location (which should be the turret)
 		bullet = b;
 		this.atk = atk;
 		this.e=e;
-		speed=15
+		speed=new Vector2f(10,10);//dummy vector
+		speed.setTheta(angle);//should calculate the speed vector's dimensions based on the angle
 		alive=true;
 		this.location=location;
 		lifespan=0;
@@ -26,10 +28,11 @@ public class Bullet {
 	
 	public void draw(Graphics g, Vector2f locationP, int angle) {
 		
+		origin=new Vector2f=locationP;
 		//This draws the bullet and rotates it on its corner before drawing the object
 		//At the location it is supposed to be at.
 		g.rotate(locationP.getX(), locationP.getY(), angle);
-		
+	
 		//Makes the image the same size as the size vector
 		g.scale(bullet.getDesign().getWidth()/bullet.getSize().getX(), bullet.getDesign().getHeight()/bullet.getSize().getY());
 		
@@ -40,13 +43,19 @@ public class Bullet {
 		g.resetTransform();
 	}
 	
-	public void update (int millis){//takes time passed as a parameter
+	public void update (int milli){//takes time passed as a parameter (not sure if this works at all)
 		if(lifespan>MAX_LIFETIME){
 			alive=false;
 		}
 		if(alive){
 			Vector2f addSpeed=speed;
-			addSpeed=addSpeed.scale(millis/1000);//should scale the new vector with the amount of time passed
+			Vector2f eLocation=e.getLocation;
+			int eX=eLocation.getX();
+			int eY=eLocation.getY();
+			int oX=origin.getX();
+			int oY=origin.getY();
+			Vector2f whereDoIGo=(eX-oX,eY-oY);//should create a vector betwen the origin and the enemy. 
+			addSpeed=whereDoIGo.scale(millis/1000);//should scale the new vector with the amount of time passed
 			location.add(addSpeed);//should add the two vectors to get a new position
 			lifespan+=millis;
 		}
@@ -58,6 +67,11 @@ public class Bullet {
 		
 		
 	}
+	
+	public Vector2f getLocation(){//returns the location vector for hitboxes
+		return location;
+	}
+	
 	
 	public int getDamage() {
 		return atk;
