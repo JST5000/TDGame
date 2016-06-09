0096854ef7cc7ad9cbc15b6c4cb3a3bcb862e992
raventooblue <ribbon.seal.90@gmail.com>
2016-06-08 17:37:24 -0700

    Update Bullet.java

--- Bullet.java
+++ Bullet.java
@@ -6,10 +6,22 @@ import org.newdawn.slick.geom.Vector2f;
 public class Bullet {
 	private Icon bullet;
 	private int atk;
+	private Enemy e;
+	private int speed;
+	private boolean alive;
+	private Vector2f location;
+	private int lifespan;
+	public static int MAX_LIFETIME;
 	
-	Bullet(Icon b, int atk) {
+	Bullet(Icon b, int atk, Enemy e, Vector2f location) {//add enemy as a parameter to lock onto, adds location (which should be the turret)
 		bullet = b;
 		this.atk = atk;
+		this.e=e;
+		speed=15
+		alive=true;
+		this.location=location;
+		lifespan=0;
+		MAX_LIFETIME=3000;
 	}
 	
 	public void draw(Graphics g, Vector2f locationP, int angle) {
@@ -26,6 +38,25 @@ public class Bullet {
 		
 		//Undoes the transformations for future drawings
 		g.resetTransform();
+	}
+	
+	public void update (int millis){//takes time passed as a parameter
+		if(lifespan>MAX_LIFETIME){
+			alive=false;
+		}
+		if(alive){
+			Vector2f addSpeed=speed;
+			addSpeed=addSpeed.scale(millis/1000);//should scale the new vector with the amount of time passed
+			location.add(addSpeed);//should add the two vectors to get a new position
+			lifespan+=millis;
+		}
+		
+	}
+	
+	public void render(GameContainer gc, Graphics g){
+		g.drawImage(bullet.getImage, location.getx(), location.getY());
+		
+		
 	}
 	
 	public int getDamage() {
