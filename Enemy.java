4c73d4f9f687c6d26553b5854dab32c3c55fbf3a
JST5000 <aim4math@gmail.com>
2016-06-14 13:45:22 -0700

    Update Enemy.java

--- Enemy.java
+++ Enemy.java
@@ -1,4 +1,5 @@
-   import org.newdawn.slick.AppGameContainer;
+package playerObjects;
+import org.newdawn.slick.AppGameContainer;
    import org.newdawn.slick.BasicGame;
    import org.newdawn.slick.Color;
    import org.newdawn.slick.GameContainer;
@@ -14,7 +15,7 @@
    
       protected boolean canFly;        //Can it fly?!!?!?!?
    
-      protected int hp;                    //Health of enemy
+      private int hp;                    //Health of enemy
    
       protected int atk;                //How much enemy damages base
    
@@ -28,49 +29,115 @@
    
       protected Icon img;                //Image for said enemy
       
-      private int reward;//reward per enemy
-   
-      private boolean alive; //whether enemy is alive or no
+      private boolean alive;
       
+      private int reward;
+      
+      private boolean killed;
+      
+      private boolean damaged;
+   
     //makes an enemy with the traits specified
-      public Enemy(boolean canFly, int hp, int atk, int def, Vector2f loc, Vector2f vel, Vector2f size, Icon img, int reward) {
+      public Enemy(boolean canFly, int hp, int atk, int def, Vector2f loc, Vector2f vel, Vector2f size, Icon img) {
          this.canFly = canFly;
-         this.hp = hp;
+         this.setHp(hp);
          this.atk = atk;
          this.def = def;
          this.loc = loc;
          this.vel = vel;
          this.size = size;
          this.img = img;
-         this.reward=reward;
-         alive=true;
+         reward = 4;
+         alive = true;
+         killed = false;
+         damaged = true;
       }
    
+      public boolean getKilled() {
+    	  return killed;
+      }
+      
+      public void setKilled(boolean b) {
+    	  killed = b;
+      }
+      
+      public boolean getDamaged() {
+    	  return damaged;
+      }
+      
+      public void setDamaged(boolean b) {
+    	  damaged = b;
+      }
+      
+      
     //Creates grid at location
-      public void render()
+      public void render(Graphics g)
       {
-			if(hp > 0)
+			if(getHp() > 0)
 			{
-         img.getDesign().draw(loc.x, loc.y);
+				g.drawImage(img.getDesign(),loc.x, loc.y);
 			}
-		else{
-			alive=false;
-		}
+			else
+			{
+				alive = false;
+			}
+      }
+      
+      public int move(Vector2f goal, int milli) {
+    	  Vector2f unit = new Vector2f(0,0);
+    	  if(loc.x != goal.x) {
+    		  if(loc.x>goal.x) {
+    			  unit.x = -1;
+    		  } else {
+    			  unit.x = 1;
+    		  }
+    	  }
+    	  
+    	  if(loc.y != goal.y) {
+    		  if(loc.y > goal.y) {
+    			  unit.y = -1;
+    		  } else {
+    			  unit.y = 1;
+    		  }
+    	  }
+    	  if(loc.y < goal.y+10 && loc.y > goal.y-10 && loc.x > goal.x-10 && loc.x < goal.x + 10) {
+    		  alive = false;
+    		  hp = 0;
+    		  return atk;
+    	  }
+    	  loc = loc.add(new Vector2f(vel.x*unit.x, vel.y*unit.y).scale((float)milli/1000));
+    	  return 0;
+      }
+      
+      public int getReward() {
+    	  return reward;
+      }
+      
+      public boolean isAlive() {
+    	  return alive;
       }
    
-      public void hit(int damage)
+      public void hit()
       {
-         hp=hp-damage;
+         setHp(getHp() - 1);
       }
        
       public boolean canFly()
       {
          return canFly;
       }
+      
+      public void setLoc(Vector2f l) {
+    	  loc = l;
+      }
+      
+      public Vector2f getLoc() {
+    	  return loc;
+      }
    
       public int hp()
       {
-         return hp;
+         return getHp();
       }
    
       public int atk()
@@ -83,17 +150,9 @@
          return def;
       }
    
-      public int getMoney(){//returns amount of money for killing enemy
-      return reward;
-      }
-      
       public Vector2f findLocation()
       {
          return loc;
-      }
-      
-      public boolean isAlive(){
-      	return alive;
       }
    
       public Icon img()
@@ -102,14 +161,15 @@
       }
       public Vector2f getDirection(Vector2f one, Vector2f two)
       {
-      	int a = ((int)one.getX);
-      	int b = ((int)one.getY);
-      	int c = ((int)two.getX);
-	int d = ((int)two.getY);
-	Vector2f left = new Vector2f((x - 1), y);
-	Vector2f right = new Vector2f((x + 1), y));
-	Vector2f up = new Vector2f(x, (y -1));
-	Vector2f down = new Vector2f(x, (y + 1));
+      	int a = ((int)one.getX());
+      	int b = ((int)one.getY());
+      	int c = ((int)two.getX());
+	int d = ((int)two.getY());
+	//I made this into unit vectors, so you multiply their value by the size of 1 grid box
+	Vector2f left = new Vector2f((-1), 0);
+	Vector2f right = new Vector2f((1), 0);
+	Vector2f up = new Vector2f(0, (-1));
+	Vector2f down = new Vector2f(0, 1);
       
 	 if(a > c)
       	 {
@@ -128,20 +188,13 @@
          return up;
       }
    }
-   public void move()
-   {
-   	if(loc.y != 8)
-   	{
-   		loc.y += 1;	
-   	}
-   	else if(loc.x > 8)
-   	{
-   		loc.x -= 1;
-   	}
-   	else
-   	{
-   		loc.x += 1;
-   	}
-   }
+
+	public int getHp() {
+		return hp;
+	}
+
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
 
    }
